fd08feda9b3c23bff8d3ec5a821f21e4c6ea30b6
AquaControl <c.edu.cunha@gmail.com>
2026-09-07 21:16:34 -0300

    Backup install before V135.3

---
+++ backups/install-before-v1353.js
@@ -0,0 +1,2 @@
+/* Backup do install.js antes da correção V135.3 dos modais. */
+/* Estado anterior carregava dashboard-modal-refino-v1352.js, que causava loop no MutationObserver ao clicar nos cards. */
